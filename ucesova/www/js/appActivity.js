823e5d87b060d8a523384b6394a73df52a27faa4
ucesova <ucesova@ucl.ac.uk>
2018-04-06 12:42:40 -0400

    nuevo intento

--- ucesova/www/js/appActivity.js
+++ ucesova/www/js/appActivity.js
@@ -49,23 +49,25 @@
 	
 	// placeholders for the L.marker and L.circle representing user's current position and accuracy    
     var current_position;
-	var current_accuracy;
+	//var current_accuracy;
 
     function onLocationFound(e) {
       // if position defined, then remove the existing position marker and accuracy circle from the map
       if (current_position) {
           mymap.removeLayer(current_position);
-          mymap.removeLayer(current_accuracy);
+          //mymap.removeLayer(current_accuracy);
       }
+
+	  
 
       var radius = e.accuracy / 2;
 
-      //current_position = L.marker(e.latlng).addTo(mymap)
+      current_position = L.circle(e.latlng,radius).addTo(mymap)
         //.bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-      current_accuracy = L.circle(e.latlng, radius).addTo(mymap);
+      //current_accuracy = L.circle(e.latlng, radius).addTo(mymap);
     }
-
+	
     function onLocationError(e) {
       alert(e.message);
     }
